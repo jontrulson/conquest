c34a947b844917b052f47d6585326aeb427107f6
jon <devnull@localhost>
1997-03-28 23:36:37 +0000

    Initial revision

---
+++ version.c
@@ -0,0 +1,14 @@
+/************************************************************************
+ *
+ * version.c - conquest's version number
+ *
+ * $Id$
+ *
+ *
+ ***********************************************************************/
+
+const char *ConquestId = "$Id$";
+const char *ConquestVersion = "$Revision$";
+const char *ConquestDate = "$Date$";
+
+/* That's it! */
