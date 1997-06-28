22737446076b0fd09ea63380f371c978accde185
jon <devnull@localhost>
1997-06-28 22:43:01 +0000

    Modified version.c to no longer depend on the RCSId keyword for it's version...

--- version.c
+++ version.c
@@ -8,7 +8,7 @@
  ***********************************************************************/
 
 const char *ConquestId = "$Id$";
-const char *ConquestVersion = "$Revision$";
-const char *ConquestDate = "$Date$";
+const char *ConquestVersion = "Version 6.4";
+const char *ConquestDate = "1997/03/28 23:34:55";
 
 /* That's it! */
