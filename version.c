c5b090b798b0937493649f7801350e66370147af
jon <devnull@localhost>
1997-12-28 21:52:12 +0000

    updated version date.

--- version.c
+++ version.c
@@ -9,6 +9,6 @@
 
 const char *ConquestId = "$Id$";
 const char *ConquestVersion = "Version 6.6";
-const char *ConquestDate = "1997/12/21 13:09:49";
+const char *ConquestDate = "1997/12/28 14:52:45";
 
 /* That's it! */
