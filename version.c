7c996f82629755fbc73213d365e55faefa7c41b4
jon <devnull@localhost>
2004-01-08 06:25:19 +0000

    fixed EXEEXT for CONQUESTSH_EXEC

--- version.c
+++ version.c
@@ -10,6 +10,6 @@
 
 const char *ConquestId = "$Id$";
 const char *ConquestVersion = "Version 8.0a";
-const char *ConquestDate = "1/2/2004";
+const char *ConquestDate = "1/7/2004";
 
 /* That's it! */
