d3354a5f40b1c93071cf79b19c871a3dbe041a94
jon <devnull@localhost>
2005-11-06 06:29:49 +0000

    - added missing sem.h and ylwrap

---
+++ sem.h
@@ -0,0 +1,19 @@
+/************************************************************************
+ *
+ * sem.h - semaphore operations on the common block.
+ *
+ * $Id$
+ *
+ * Copyright 1999-2005 Jon Trulson under the ARTISTIC LICENSE. (See LICENSE).
+ ***********************************************************************/
+
+#ifndef __SEM_H
+#define __SEM_H
+
+char *semGetName(int what);
+int semInit(void);
+void Lock(int what);
+void Unlock(int what);
+char *semGetStatusStr(void);
+
+#endif /* __SEM_H */
