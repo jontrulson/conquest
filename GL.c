0449182e175babaf124bc1f602097f921286aec3
jon13 <devnull@localhost>
2009-09-12 06:12:17 +0000

    - ahem.  use the *viewer* aspect, not the window aspect.  dumbass. :)

--- GL.c
+++ GL.c
@@ -1837,7 +1837,7 @@ resize(int w, int h)
   
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluPerspective(VIEWANGLE, dConf.wAspect, 
+  gluPerspective(VIEWANGLE, dConf.vAspect, 
                  1.0, 1000.0);
 
   /* save a copy of this matrix */
