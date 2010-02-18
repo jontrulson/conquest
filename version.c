f6131c1151711cd9987544c1a425a48c1a3e4a4b
jon13 <devnull@localhost>
2010-02-18 22:43:58 +0000

    - bump version 8.4.1g, for kicks

--- version.c
+++ version.c
@@ -9,7 +9,7 @@
  ***********************************************************************/
 
 const char *ConquestId = "$Id$";
-const char *ConquestVersion = "Version 8.4.1f";
-const char *ConquestDate = "02/05/2010";
+const char *ConquestVersion = "Version 8.4.1g";
+const char *ConquestDate = "02/18/2010";
 
 /* That's it! */
