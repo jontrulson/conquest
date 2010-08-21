efddcb6f48afc55d7b411b176e97f6988cdb70f4
Jon Trulson <jon@radscan.com>
2010-08-20 19:11:15 -0600

    Add size member to Planet_t

--- planet.h
+++ planet.h
@@ -14,18 +14,19 @@
 typedef struct {
   real x;			/* x coordinates of planet */
   real y;			/* y coordinates of planet */
-  real orbrad;			/* radius of the orbit */
-  real orbvel;			/* velocity of the orbit in deg/min */
-  real orbang;			/* current angle of the orbit */
-  int  primary;			/* planet this planet orbits */
-  int  type;			/* type of planet - M, dead, sun */
-  int  real;			/* tells if planet can be seen */
-  int  team;			/* which team owns the planet */
-  int  armies;			/* how many armies are on the planet */
-  int  uninhabtime;		/* minutes left till inhabitable */
+  real orbrad;                  /* radius of the orbit */
+  real orbvel;                  /* velocity of the orbit in deg/min */
+  real orbang;                  /* current angle of the orbit */
+  real size;                    /* size of planet in CU's */
+  int  primary;                 /* planet this planet orbits */
+  int  type;                    /* type of planet - M, dead, sun */
+  int  real;                    /* tells if planet can be seen */
+  int  team;                    /* which team owns the planet */
+  int  armies;                  /* how many armies are on the planet */
+  int  uninhabtime;             /* minutes left till inhabitable */
   int  scanned[NUMPLAYERTEAMS];	/* which planets have been */
 				/* scanned by which teams */
-  char name[MAXPLANETNAME];	/* planet name */
+  char name[MAXPLANETNAME];     /* planet name */
 } Planet_t;
 
 #endif /* PLANET_H_INCLUDED */
