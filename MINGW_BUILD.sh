d6fb1a34baf5196e3498940d0169a4359773bbe5
Jon Trulson <jon@radscan.com>
2011-06-12 21:15:46 -0600

    New command to build windows binaries for new machine

--- MINGW_BUILD.sh
+++ MINGW_BUILD.sh
@@ -1,5 +1,4 @@
 # This is how I build it using a mingw cross compiler on my system.  YMMV.
 set -x
-./configure --prefix=/tmp --disable-shared --build=i686-linux --host=i686-pc-mingw32 --with-sdl-prefix=/home/jon/mingw32
 
-
+./configure --prefix=/tmp --disable-shared --build=i686-linux --host=i586-mingw32msvc --with-sdl-prefix=/home/jon/mingw32 CFLAGS="-I/home/jon/mingw32/include" LDFLAGS="-L/home/jon/mingw32/lib"
