1bfc4444c7b22c23c51e1813737cc0201e935de5
Jon Trulson <jon@radscan.com>
2017-05-27 20:48:22 -0600

    remove ratfor.c, no longer needed
    ratfor.c was a file created during the initial port from RATFOR
    (RATional FORtran) to C.  It implemented certain functionality that
    seemd to be intrinsic to RATFOR.
    
    Now that we've removed the need for or re-implemented elsewhere
    those RATFOR functions, we can finally remove the ratfor.c file
    altogether.  Only took about 20 years. :)

--- ratfor.c
+++
@@ -1,9 +0,0 @@
-/************************************************************************
- * Functions that appear to be intrinsic to RATFOR
- *
- * Copyright Jon Trulson under the ARTISTIC LICENSE. (See LICENSE).
- ***********************************************************************/
-
-#include "c_defs.h"		/* includes everthing else */
-#include "conqutil.h"
-
