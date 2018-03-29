c76588bf4fad6ab89f875a7880de1f90719111d7
Jon Trulson <jon@radscan.com>
2018-03-28 19:03:30 -0600

    server.h: enumerate and remove unused states
    Use an enum to represent states, and remove unused ones.
    
    Also use bools in ServerInfo_t rather than ints where appropriate.
    
    Deleted a lost comment.

--- src/server.h
+++ src/server.h
@@ -36,29 +36,25 @@
 
 #include "sproc.h"
 
-#define SVR_STATE_NONE      0x00000000	/* local init */
-#define SVR_STATE_PREINIT   0x00000001	/* local init */
-#define SVR_STATE_INIT      0x00000002	/* negotiate with client/login */
-#define SVR_STATE_MAINMENU  0x00000004	/* in main menu */
-#define SVR_STATE_PLAYMENU  0x00000008	/* a menu/info screen engaged
-					   during play */
-#define SVR_STATE_PLAY      0x00000010	/* playing */
-#define SVR_STATE_TERMINATE 0x00000020	/* terminating */
+typedef enum {
+    SVR_STATE_NONE = 0,                /* nothing */
+    SVR_STATE_PREINIT,                 /* local init */
+    SVR_STATE_INIT,                    /* negotiate with client/login */
+    SVR_STATE_MAINMENU,                /* in main menu */
+    SVR_STATE_PLAY,                    /* playing */
+} serverStates_t;
 
 typedef struct {
-    int     sock;			/* socket to client */
-    int     usock;                /* udp socket to client */
-    uint16_t listenPort;          // our listen port.  Ahem.
+    int sock;                     /* socket to client */
+    int usock;                    /* udp socket to client */
+    uint16_t listenPort;          /* our listen port */
     struct  sockaddr_in clntaddr; /* client's saddrin struct */
-    int     doUDP;                /* send udp? */
-    uint32_t state;		/* current state */
-    uint32_t clientDead;		/* is the client dead/ */
-    uint32_t isMaster;		/* is the Maseter server? */
-    char    remotehost[MAXHOSTNAME]; /* self evident */
+    bool doUDP;                   /* send udp? */
+    serverStates_t state;         /* current state */
+    uint32_t clientDead;          /* is the client dead/ */
+    bool isMaster;                /* is the Maseter server? */
+    char remotehost[MAXHOSTNAME]; /* self evident */
 } ServerInfo_t;
-
-
-/* we also want to keep a copy of all packets sent */
 
 #ifdef NOEXTERN_SERVER
 ServerInfo_t sInfo;
@@ -67,7 +63,7 @@ extern ServerInfo_t sInfo;
 #endif
 
 int sendClientStat(int sock, uint8_t flags, int16_t snum, uint8_t team,
-		   uint16_t unum, uint8_t esystem);
+                   uint16_t unum, uint8_t esystem);
 int sendUser(int sock, uint16_t unum);
 int sendShip(int sock, uint8_t snum);
 int sendPlanet(int sock, uint8_t pnum, int force);
