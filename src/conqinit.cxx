bfa91d3a76a1ac27ce4a9e4b85348fde2c8c539c
Jon Trulson <jon@radscan.com>
2019-01-10 13:09:13 -0700

    conqinit: fix up some issues
    This utility missed some updates based on previous work.  So, fix up
    the options supported, correct a few bugs, and update the man page.

--- src/conqinit.cxx
+++ src/conqinit.cxx
@@ -46,23 +46,20 @@
 /* print_usage - print usage. Duh. */
 void print_usage()
 {
-    fprintf(stderr, "Usage: conqinit [-f <file>] [-vdD] \n");
-    fprintf(stderr, "       conqinit -f <file> -h [-vd] \n\n");
+    fprintf(stderr, "Usage: conqinit [-f <file>] [-v] [-D] [-t | -s] [-G <subdir>] \n");
     fprintf(stderr, "\t-G <subdir>   specify alternate game subdirectory\n");
     fprintf(stderr, "\t-f <file>     read from <file>.\n");
-    fprintf(stderr, "\t-v            be verbose about everything.\n");
-    fprintf(stderr, "\t-d            turns on debugging.\n");
+    fprintf(stderr, "\t-v            increase verbosity level by 1.\n");
     fprintf(stderr, "\t-t            parse texture data instead of conqinit data\n");
     fprintf(stderr, "\t-s            parse sound data instead of conqinit data\n");
-    fprintf(stderr, "\t-D            dump current universe (global, shiptypes,"
+    fprintf(stderr, "\t-D            dump current universe (global, shiptypes,\n"
                     "                planets) to stdout in conqinitrc format\n");
 }
 
 
 int main(int argc, char **argv)
 {
-    extern char *optarg;
-    int debuglevel = 0, verbosity = 0, ftype = CQI_FILE_CONQINITRC;
+    int verbosity = 0, ftype = CQI_FILE_CONQINITRC;
     int ch;
     char *filenm = NULL;
 
@@ -71,14 +68,12 @@ int main(int argc, char **argv)
     utSetLogConfig(false, true);    /* log + stderr! :) */
     utSetLogProgramName(argv[0]);
 
-    while ( (ch = getopt( argc, argv, "vdDf:ts" )) != EOF )
-    {      switch(ch)
+    while ( (ch = getopt( argc, argv, "Df:G:stv" )) != EOF )
+    {
+        switch(ch)
 	{
 	case 'v':
-            verbosity = true;
-            break;
-        case 'd':
-            debuglevel++;
+            verbosity++;
             break;
         case 'D':
             dumpUniverse();
@@ -89,9 +84,14 @@ int main(int argc, char **argv)
             break;
         case 't':
             ftype = CQI_FILE_TEXTURESRC_ADD;
+            verbosity++;        // get stats on this option
             break;
         case 's':
             ftype = CQI_FILE_SOUNDRC_ADD;
+            verbosity++;        // get stats on this option
+            break;
+        case 'G':
+            gameSubdirectory.set(optarg);
             break;
 	default:
             print_usage();
@@ -101,7 +101,7 @@ int main(int argc, char **argv)
 
     if (cqiLoadRC(ftype, filenm, verbosity))
     {
-        fprintf(stderr, "conqinit: FATAL: load failed.\n");
+        fprintf(stderr, "conqinit: FATAL: cqiLoadRC() failed.\n");
         return 1;
     }
 
